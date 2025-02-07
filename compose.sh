dfb5ac5bf0eb1a7ab068a4c674ebc6a7c970bb17
Scirelgar <81189385+Scirelgar@users.noreply.github.com>
2025-02-07 14:19:44 -0500

    cleaning up and commenting

--- compose.sh
+++ compose.sh
@@ -29,6 +29,7 @@ while getopts "sc:" opt; do
     esac
 done
 
+# Define sed_i function for in-place editing
 if [ "$(uname)" == "Linux" ]; then
     sed_i () {
         sed -i "$@"
@@ -41,32 +42,37 @@ fi
 
 VERSION=scan
 
+# Create top level folder for scan
 FOLDER=magic_castle-$VERSION
-
 mkdir -p $FOLDER
+
 for provider in "${CLOUD[@]}"; do
+    # Create folder for each provider
     cur_folder=$FOLDER/magic_castle-$provider-$VERSION
     mkdir -p $cur_folder
+    # Copy files
     cp -RfL common $cur_folder/
     rm $cur_folder/common/{variables,outputs}.tf
     cp -RfL dns $cur_folder/
     cp -RfL $provider $cur_folder/
-    cp -fL examples/$provider/main.tf $cur_folder
     mv $cur_folder/$provider/README.md $cur_folder
+    cp LICENSE $cur_folder
+    # In module $provider, replace source and config_version
+    cp -fL examples/$provider/main.tf $cur_folder
     sed_i 's;git::https://github.com/ComputeCanada/magic_castle.git//;./;g' $cur_folder/main.tf
     sed_i "s;\"main\";\"$VERSION\";" $cur_folder/main.tf
-    cp LICENSE $cur_folder
 
-    ## Initialize to create .terraform.lock.hcl file
+    # Editing main.tf so that all modules are scanned
     cd $cur_folder
-    ## Make sure only one module is named "dns"
+    # Make sure only one module is named "dns"
     sed_i '1,/dns/s/dns/cloudflare/' main.tf
-    ## Uncomment DNS modules
+    # Uncomment DNS modules
     sed_i 's/^#\ //g' main.tf
     cd -
 
-    ## Recreate a new unmodified main.tf
+    # Recreate a new unmodified main.tf
     cp -fL examples/$provider/main.tf $cur_folder
+    # In module $provider, replace source and config_version
     sed_i 's;git::https://github.com/ComputeCanada/magic_castle.git//;./;g' $cur_folder/main.tf
     sed_i "s;\"main\";\"$VERSION\";" $cur_folder/main.tf
 done
